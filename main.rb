f8bf4f7b6eee37d180dadf3ef93cf2b568c68a52
ElAtrash <bioelatrash@gmail.com>
2019-11-10 01:32:46 +0300

    improve code and code quality

--- main.rb
+++ main.rb
@@ -3,60 +3,62 @@ require "nokogiri"
 require "open-uri"
 
 # replace ur_token with a token from botfather
-TOKEN = "ur_token"
+TOKEN = ""
 
-url = Nokogiri::HTML.parse(open("http://www.sigaretnet.by/aktsii.html"))
-url_pref = "http://www.sigaretnet.by"
-xpath_d = url.xpath("//div[@class = 'img-intro-left']/img/@src")[1].text.gsub(' ', '%20')
-DAILY_SALE = url_pref + xpath_d
+module LiquidSales
+  class Sigaretnet
+    XPATH_WEEK = '//div[@class="jcomments-links"]/a[contains(@title, "Товары недели")]/@href'
 
-xpath_dayinfo = url.xpath("//a[@class='readmore-link']/@href")[1].text
-DAY_INFO = url_pref + xpath_dayinfo
+    def initialize
+      @site_name = "http://www.sigaretnet.by"
+      @sales_page = Nokogiri::HTML.parse(open("http://www.sigaretnet.by/aktsii.html"))
+    end
 
-xpath_w = url.xpath("//div[@class = 'img-intro-left']/img/@src")[2].text.gsub(' ', '%20')
-WEEK_SALE = url_pref + xpath_w
+    def get_week_sale_url
+      @site_name + @sales_page.xpath(XPATH_WEEK).text
+    end
 
-xpath_weekinfo = url.xpath("//a[@class='readmore-link']/@href")[2].text
-WEEK_INFO = url_pref + xpath_weekinfo
-
-Telegram::Bot::Client.run(TOKEN) do |bot|
-  bot.listen do |message|
-    case message.text
-    when "/start"
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "Здравствуй, #{message.from.first_name}.\n/help для навигации."
-        )
-    when "/about_day"
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "#{DAY_INFO}"
-        )
-    when "/day"
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "#{DAILY_SALE}"
-        )
-    when "/about_week"
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "#{WEEK_INFO}"
-        )
-    when "/week"
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "#{WEEK_SALE}"
-        )
-    when "/help"
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "/day - чтобы узнать ежедневную акцию.\n/about_day - ссылка на страницу акции.\n/week - чтобы узнать недельную акцию.\n/about_week- ссылка на страницу недельной акции.\n/help - для навигации."
-        )
-    else
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "/help для навигации."
-        )
+    def parse_week_sales
+      week_sales = Nokogiri::HTML.parse(open(get_week_sale_url))
+      items_img_urls = week_sales.xpath('//div[@class="item-pageakcii-p"]//p/img/@src').map do |img_url|
+        @site_name + img_url.text
+      end
+      items_img_urls
     end
   end
+
+  class SigaretnetTelegramBot
+    def initialize
+      @sigaretnet = Sigaretnet.new
+    end
+
+    def run
+      Telegram::Bot::Client.run(TOKEN) do |bot|
+        bot.listen do |message|
+          case message.text
+          when "/start"
+            bot.api.send_message(
+              chat_id: message.chat.id,
+              text: "Здравствуй, #{message.from.first_name}.\n/help для навигации."
+            )
+          when "/week"
+            @sigaretnet.parse_week_sales.each {|img| bot.api.send_message(chat_id: message.chat.id, text: img) }
+          when "/help"
+            bot.api.send_message(
+              chat_id: message.chat.id,
+              text: "/week - чтобы узнать недельную акцию.\n/about_week- ссылка на страницу недельной акции.\n/help - для навигации."
+             )
+          else
+            bot.api.send_message(
+              chat_id: message.chat.id,
+              text: "/help для навигации."
+             )
+          end
+        end
+      end
+    end # run  
+  end
 end
+
+sigaretnet_bot = LiquidSales::SigaretnetTelegramBot.new
+sigaretnet_bot.run
